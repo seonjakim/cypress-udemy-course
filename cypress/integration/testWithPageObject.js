86fe3e2aaa67f3ce1edf1b511390a58772fb40a2
seonja <seonja.kim0@gmail.com>
2022-05-08 20:21:18 +0900

    cluster method into class for better readablity

--- cypress/integration/testWithPageObject.js
+++ cypress/integration/testWithPageObject.js
@@ -1,4 +1,7 @@
+import { onDatePickerPage } from "../support/page_objects/datepickerPage";
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage";
 import { navigateTo } from "../support/page_objects/navigationPage";
+import { onSmartTablePage } from "../support/page_objects/smartTablePage";
 
 describe("Test with Page Objects", () => {
   beforeEach("open application", () => {
@@ -11,5 +14,26 @@ describe("Test with Page Objects", () => {
     navigateTo.smartTablePage();
     navigateTo.tooltipPage();
     navigateTo.toasterPage();
+  });
+
+  it("should submit Inline and Basic from and select tmr date in the calendar", () => {
+    navigateTo.formLayoutsPage();
+    onFormLayoutsPage.submitInlineFormWithNameAndEmail(
+      "Artem",
+      "test@test.com"
+    );
+    onFormLayoutsPage.submitBasicFormWithEmailAndPassword(
+      "test@test.com",
+      "password"
+    );
+
+    navigateTo.datepickerPage();
+    onDatePickerPage.selectCommonDatepickerDateFromToday(1);
+    onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14);
+
+    navigateTo.smartTablePage();
+    onSmartTablePage.addNewRecordWithFirstAndLastName("Artem", "Bondar");
+    onSmartTablePage.updateAgeByFirstName("Artem", "35");
+    onSmartTablePage.deleteRowByIndex(1);
   });
 });
