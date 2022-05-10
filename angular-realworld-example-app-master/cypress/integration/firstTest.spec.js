b4ff0dfbf676576aaf2f22b32c33891d376773af
seonja <seonja.kim0@gmail.com>
2022-05-10 10:16:34 +0900

    verification of the browser API calls

--- angular-realworld-example-app-master/cypress/integration/firstTest.spec.js
+++ angular-realworld-example-app-master/cypress/integration/firstTest.spec.js
@@ -5,7 +5,27 @@ describe("Test with backend", () => {
     cy.loginToApplication();
   });
 
-  it("should log in", () => {
-    cy.log("yeey we logged in");
+  it("verify correct request and response", () => {
+    // below deplicated methods
+    cy.server(); // create server
+    cy.route("POST", "**/articles").as("postArticles"); // post /articles endpoint api and save the data as 'postArticles'
+
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("This is a title");
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the Article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles"); // wait till the call is completed
+    cy.get("@postArticles").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.status).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the Article"
+      );
+      expect(xhr.reponse.body.article.description).to.equal(
+        "This is a description"
+      );
+    });
   });
 });
